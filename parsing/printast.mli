028086cc6060da1f814ca0bf3fa59fd7d259d1e3
kakadu <kakadu.hafanana@gmail.com>
2017-10-10 20:54:07 +0300

    Allow to use Printast.core_type

--- parsing/printast.mli
+++ parsing/printast.mli
@@ -17,6 +17,7 @@ val interface : formatter -> signature_item list -> unit;;
 val implementation : formatter -> structure_item list -> unit;;
 val top_phrase : formatter -> toplevel_phrase -> unit;;
 
+val core_type: int -> formatter -> core_type -> unit
 val expression: int -> formatter -> expression -> unit
 val structure: int -> formatter -> structure -> unit
 val payload: int -> formatter -> payload -> unit
